c2d225dfe4ef1233fd1ad841bdffc06fb8de76eb
hackerudroids <hackerudroids@gmail.com>
2017-06-22 20:53:03 +0300

    Ver 01.04

--- app/src/main/java/com/arichafamily/shoppingfun/MainActivity.java
+++ app/src/main/java/com/arichafamily/shoppingfun/MainActivity.java
@@ -1,6 +1,8 @@
 package com.arichafamily.shoppingfun;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,6 +11,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,6 +22,8 @@ import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
@@ -27,10 +33,26 @@ public class MainActivity extends AppCompatActivity
     FirebaseUser mUser;
     FirebaseAuth mAuth;
 
+    void sha1() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.arichafamily.shoppingfun", PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA1");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         mAuth.addAuthStateListener(mAuthListener);
+        sha1();
     }
 
     @Override
