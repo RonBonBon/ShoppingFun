68ca093ef8622bb75b4d51947c7dc7cdb9b32bbf
hackeru <hackerudroids@gmail.com>
2017-06-26 19:51:56 +0300

    Ver 01.12

--- app/src/main/java/com/arichafamily/shoppingfun/dialogs/AddListDialogFragment.java
+++ app/src/main/java/com/arichafamily/shoppingfun/dialogs/AddListDialogFragment.java
@@ -1,17 +1,19 @@
 package com.arichafamily.shoppingfun.dialogs;
 
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.arichafamily.shoppingfun.R;
 import com.arichafamily.shoppingfun.models.UserList;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -23,9 +25,6 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import mehdi.sakout.fancybuttons.FancyButton;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AddListDialogFragment extends DialogFragment {
 
 
@@ -64,13 +63,36 @@ public class AddListDialogFragment extends DialogFragment {
             Log.e("Tomer", "No User!");
             return; //No User -> No DB
         }
+
+        if (etListName.getText().length() == 0)
+        {
+            etListName.setError("Empty list name...");
+            return;
+        }
         //2) listID = ref UserListTable -> UID -> push
         DatabaseReference newUserListRowRef = FirebaseDatabase.getInstance()
                 .getReference("UserLists")
                 .child(currentUser.getUid())
                 .push();
         //3) create a new UserList Model
-        UserList list = new UserList();
+        UserList list = new UserList(etListName.getText().toString(),
+                currentUser.getUid(),
+                currentUser.getPhotoUrl().toString(),
+                newUserListRowRef.getKey());
         //4) ref.setValue(userList)
+        newUserListRowRef.setValue(list).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getContext(), "Saved!", Toast.LENGTH_SHORT).show();
+                dismiss();
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                dismiss();
+            }
+        });
     }
 }
